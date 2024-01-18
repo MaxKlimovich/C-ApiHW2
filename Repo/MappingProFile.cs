8b2d801b607280b5f856e8714129c4555989e680
Henry Bronx <klimovich.maks@gmail.com>
2024-01-18 14:22:01 +0300

    add Repo HW2

---
+++ Repo/MappingProFile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using WebApplication.Models;
+using WebApplication.Models.DTO;
+using WebApplication.Models;
+
+
+namespace WebApplication.Repo
+{
+    public class MappingProFile : Profile
+    {
+        public MappingProFile()     
+        {
+            CreateMap<Product,ProbuctDto>(MemberList.Destination).ReverseMap();
+            CreateMap<Group,GroupDto>(MemberList.Destination).ReverseMap();
+            CreateMap<Store,StoreDto>(MemberList.Destination).ReverseMap();    
+        }
+    }
+}
