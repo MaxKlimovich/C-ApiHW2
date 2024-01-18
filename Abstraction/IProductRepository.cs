279e5f876d48ce592e8248b854ebd158fd26ea61
Henry Bronx <klimovich.maks@gmail.com>
2024-01-18 14:18:41 +0300

    add Abstraction HW2

---
+++ Abstraction/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using WebApplication.Models;
+using WebApplication.Models.DTO;
+using WebApplication.Models.DTO;
+
+namespace WebApplication.Abstraction
+{
+
+    public interface IProductRepository
+    {
+        public int AddGroup(GroupDto group);
+
+        public IEnumerable<GroupDto> GetGroups();
+
+        public int AddProduct(ProbuctDto product);
+
+        public IEnumerable<ProbuctDto> GetProducts();
+
+    }
+}
